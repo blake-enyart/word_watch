dff97f261988dcfb96787c6a5d4c4d1c7bf5f749
Blake <blake.enyart@gmail.com>
2019-07-18 11:40:26 -0600

    WIP: Update top word in jQuery

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  $.ajax('http://localhost:3000/api/v1/top_word')
-  .done((res) => {
-    $('.top-word > h3').append(Object.keys(res.word)[0])
-    $('.word-count').append("<h3>".concat(Object.values(res.word)[0],"</h3>"))
-  })
+  function topWord() {
+    var topWord = 'Top word from Word Watch API:'
+    $.ajax('http://localhost:3000/api/v1/top_word')
+    .done((res) => {
+      $('.top-word > h3').replaceWith("<h3>".concat(topWord.concat(
+        Object.keys(res.word)[0]
+      ), "</h3>"));
+
+      $('.word-count').replaceWith(
+        "<h3>".concat(
+          Object.values(res.word)[0],"</h3>"
+        )
+      )
+    });
+  };
+  topWord();
+
+  $('.text-submission > button').click(() => {
+    var wordList = $('.text-submission > textarea').val().split(' ');
+    for(var i = 0; i < wordList.length; i++) {
+      var body = { "word": { "value": wordList[i] } }
+      $.post('http://localhost:3000/api/v1/words', body)
+    }
+  });
 })
