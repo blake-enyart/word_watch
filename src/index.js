59ae11af7b87dbeb7881d0009d1d7ac85f420d19
Blake <blake.enyart@gmail.com>
2019-07-18 10:58:55 -0600

    WIP: Append top word to DOM

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import $ from 'jquery'
 
 $(document).ready(() => {
-  // have fun!
+  $.ajax('http://localhost:3000/api/v1/top_word')
+  .done((res) => {
+    $('.top-word > h3').append(Object.keys(res.word)[0])
+  })
 })
