9cb88c5a5aaa4e30c59cdd00128040b1074b01a2
Blake <blake.enyart@gmail.com>
2019-07-18 11:04:08 -0600

    WIP: Append word count to box below top word

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ $(document).ready(() => {
   $.ajax('http://localhost:3000/api/v1/top_word')
   .done((res) => {
     $('.top-word > h3').append(Object.keys(res.word)[0])
+    $('.word-count').append("<h3>".concat(Object.values(res.word)[0],"</h3>"))
   })
 })
